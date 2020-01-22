7f6f659304dec57c4a9279ffd5864250cd3c0b86
wekoil_desktop <michaj24@fit.cvut.cz>
2020-01-22 11:56:52 +0100

    loading mail from config

--- mail.py
+++ mail.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import configparser
 
 def send_mail():
 
@@ -6,10 +7,23 @@ def send_mail():
     smtp_server = "smtp.gmail.com"
     sender_email = "mjan72771@gmail.com"
     receiver_email = "mjan72771@gmail.com"
-    password = input("Type your password and press enter: ")
+
+    config = configparser.ConfigParser()
+
+    with open('mail.cfg') as f:
+        config.read_file(f)
+
+    port = config['email']['port']
+    smtp_server = config['email']['smtp']
+    sender_email = config['email']['sender']
+    receiver_email = config['email']['receiver']
+
+    # password = input("Type your password and press enter:")
+    with open('gmail_password.txt') as f:  
+        password = f.read()
     message = """Subject: Hi there
 
-This message is sent from Python."""
+    This message is sent from Python."""
 
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
