07059a4246f1c0f2c1f39628f3949d84baa4a45b
wekoil_desktop <michaj24@fit.cvut.cz>
2020-01-28 18:58:48 +0100

    tabs to spaces

--- message.py
+++ message.py
@@ -13,97 +13,94 @@ CONFIG_FILE = 'config.cfg'
 
 class Message:
     __metaclass__ = ABCMeta
-
+    
     @abstractmethod
     def send(self): raise NotImplementedError
 
 class Mail(Message):
-	def send(subject=None, message=None, receiver=None):
-	    config = configparser.ConfigParser()
+    def send(subject=None, message=None, receiver=None):
+        config = configparser.ConfigParser()
+        with open(CONFIG_FILE) as f:
+            config.read_file(f)
 
-	    with open(CONFIG_FILE) as f:
-	        config.read_file(f)
+        port = config['email']['port']
+        smtp_server = config['email']['smtp']
+        sender_email = config['email']['sender']
+        receiver_email = config['email']['receiver']
 
-	    port = config['email']['port']
-	    smtp_server = config['email']['smtp']
-	    sender_email = config['email']['sender']
-	    receiver_email = config['email']['receiver']
+        with open('gmail_password.txt') as f:  
+            password = f.read()
 
-	    with open('gmail_password.txt') as f:  
-	        password = f.read()
+        if message == None:
+            message = 'Default mail body.'
 
-	    if message == None:
-		    message = 'Default mail body.'
+        mail_message = MIMEMultipart("alternative")
+        mail_message["Subject"] = subject
+        mail_message["From"] = sender_email
+        mail_message["To"] = receiver_email
+        part = MIMEText(message, "plain")
+        mail_message.attach(part)
 
-	    mail_message = MIMEMultipart("alternative")
-	    mail_message["Subject"] = subject
-	    mail_message["From"] = sender_email
-	    mail_message["To"] = receiver_email
-	    part = MIMEText(message, "plain")
-	    mail_message.attach(part)
-
-	    context = ssl.create_default_context()
-	    with smtplib.SMTP(smtp_server, port) as server:
-	        server.ehlo()  # Can be omitted
-	        server.starttls(context=context)
-	        server.ehlo()  # Can be omitted
-	        server.login(sender_email, password)
-	        server.sendmail(sender_email, receiver_email, mail_message.as_string())
-
-	    # print('Mail sent.')
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, mail_message.as_string())
 
 class Slack(Message):
-	def send(message=None, channel=None):
+    def send(message=None, channel=None):
 
-		with open('slack_token.txt') as f:  
-		    slack_token = f.read()
-		client = slack.WebClient(token=slack_token)
+        with open('slack_token.txt') as f:  
+            slack_token = f.read()
+        client = slack.WebClient(token=slack_token)
 
-		if message == None:
-			message = "Hello from your app! :tada:"
+        if message == None:
+            message = "Hello from your app! :tada:"
 
-		if channel == None:
-			config = configparser.ConfigParser()
-			with open(CONFIG_FILE) as f:
-				config.read_file(f)
-			channel = config['slack']['channel']
+        if channel == None:
+            config = configparser.ConfigParser()
+            with open(CONFIG_FILE) as f:
+                config.read_file(f)
+            channel = config['slack']['channel']
 
 
-		client.chat_postMessage(
-		  channel=channel,
-		  text=message
-		)
+        client.chat_postMessage(
+          channel=channel,
+          text=message
+        )
 
 class WhatsApp(Message):
-	def send(receiver=None, message=None):
-		config = configparser.ConfigParser()
-		with open('twilio.cfg') as f:
-			config.read_file(f)
-		sid = config['twilio']['SID']
-		token = config['twilio']['TOKEN']
+    def send(receiver=None, message=None):
+        config = configparser.ConfigParser()
+        with open('twilio.cfg') as f:
+            config.read_file(f)
+        sid = config['twilio']['SID']
+        token = config['twilio']['TOKEN']
 
-		# client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
-		client = Client(sid, token)
+        # client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
+        client = Client(sid, token)
 
-		with open(CONFIG_FILE) as f:
-			config.read_file(f)
-		sender = config['twilio']['sender']
+        with open(CONFIG_FILE) as f:
+            config.read_file(f)
+        sender = config['twilio']['sender']
 
-		# this is the Twilio sandbox testing number
-		from_whatsapp_number='whatsapp:{}'.format(sender)
+        # this is the Twilio sandbox testing number
+        from_whatsapp_number='whatsapp:{}'.format(sender)
 
-		if receiver == None:
-			receiver = config['twilio']['receiver']
+        if receiver == None:
+            receiver = config['twilio']['receiver']
 
-		# replace this number with your own WhatsApp Messaging number
-		to_whatsapp_number='whatsapp:{}'.format(receiver)
+        # replace this number with your own WhatsApp Messaging number
+        to_whatsapp_number='whatsapp:{}'.format(receiver)
 
-		if message == None:
-			message = 'Nazdar'
+        if message == None:
+            message = 'Nazdar'
 
-		client.messages.create(body=message,
-		                       from_=from_whatsapp_number,
-		                       to=to_whatsapp_number)
+        client.messages.create(body=message,
+                               from_=from_whatsapp_number,
+                               to=to_whatsapp_number)
 
 
 if __name__ == '__main__':
